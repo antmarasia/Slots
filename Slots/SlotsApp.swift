dab402f5379ff37822f5d7c0e0c9800a5264c27d
Anthony <a>
2021-04-26 16:59:27 -0400

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Anthony Marasia on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

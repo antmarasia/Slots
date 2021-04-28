34412b99f2dca4ff4f67149d68758d07e07793c9
antmarasia <antmarasia@users.noreply.github.com>
2021-04-28 15:52:37 -0400

    Refactor to MVVM

--- Slots/GameView.swift
+++ Slots/GameView.swift
@@ -9,12 +9,7 @@ import SwiftUI
 
 struct GameView: View {
     
-    @State var credits = 1000
-    let images = ["apple", "cherry", "star"]
-    
-    @State var slot1 = "apple"
-    @State var slot2 = "apple"
-    @State var slot3 = "apple"
+    @ObservedObject var slotViewModel = SlotViewModel()
     
     var body: some View {
         VStack {
@@ -22,32 +17,26 @@ struct GameView: View {
                 .font(.largeTitle)
                 .padding(.top, 20)
             Spacer()
-            Text("Credits: \(credits)")
+            Text("Credits: \(slotViewModel.credits)")
             Spacer()
             HStack {
                 Spacer()
-                Image(slot1)
+                Image(slotViewModel.slot1)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 Spacer()
-                Image(slot2)
+                Image(slotViewModel.slot2)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 Spacer()
-                Image(slot3)
+                Image(slotViewModel.slot3)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                 Spacer()
             }
             Spacer()
             Button("Spin") {
-                if credits <= 0 {
-                    return
-                }
-                
-                spin()
-                
-                checkScore()
+                slotViewModel.spin()
             }
             .frame(width: 100.0, height: 40.0)
             .background(Color.pink)
@@ -59,27 +48,6 @@ struct GameView: View {
         }
     }
     
-    private func getRandomNum() -> Int {
-        return Int.random(in: 0...2)
-    }
-    
-    private func spin() {
-        let rand1 = getRandomNum()
-        let rand2 = getRandomNum()
-        let rand3 = getRandomNum()
-        
-        slot1 = images[rand1]
-        slot2 = images[rand2]
-        slot3 = images[rand3]
-    }
-    
-    private func checkScore() {
-        if slot1 == slot2 && slot1 == slot3 {
-            credits += 5
-        } else {
-            credits -= 5
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Slots/SlotViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  SlotViewModel.swift
+//  Slots
+//
+//  Created by Anthony Marasia on 4/28/21.
+//
+
+import Foundation
+
+final class SlotViewModel : ObservableObject {
+    
+    private let images = ["apple", "cherry", "star"]
+    
+    @Published private(set) var slot1 = "apple"
+    @Published private(set) var slot2 = "apple"
+    @Published private(set) var slot3 = "apple"
+    @Published private(set) var credits = 1000
+    
+    public func spin() {
+        print("Spinning")
+        if credits <= 0 {
+            return
+        }
+        doSpin()
+        checkScore()
+    }
+    
+    private func getRandomNum() -> Int {
+        return Int.random(in: 0...2)
+    }
+    
+    private func doSpin() {
+        slot1 = images[getRandomNum()]
+        slot2 = images[getRandomNum()]
+        slot3 = images[getRandomNum()]
+    }
+    
+    private func checkScore() {
+        if slot1 == slot2 && slot1 == slot3 {
+            credits += 5
+        } else {
+            credits -= 5
+        }
+    }
+}
